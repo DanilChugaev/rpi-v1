63d2e25edeeb532f370c1c3af1b71165352148c9
danilchugaev <dmchugaev@gmail.com>
2021-01-15 13:52:52 +0700

    upd comment

--- Digit.js
+++ Digit.js
@@ -22,36 +22,41 @@ module.exports = class Digit {
      * #   #
      * #   #
      * # # #
-     * 
-     * @return {Array<Number>}
+     *
+     * @return {Array<number>}
      */
     zero({ x = 0, y = 0 } = {}) {
-        this.pixels[this.index({ x: x + 0, y: y + 0 })] = this.color;
-        this.pixels[this.index({ x: x + 1, y: y + 0 })] = this.color;
-        this.pixels[this.index({ x: x + 2, y: y + 0 })] = this.color;
+        this.pixels[this.index({ x: x, y: y })] = this.color;
+        this.pixels[this.index({ x: x + 1, y: y })] = this.color;
+        this.pixels[this.index({ x: x + 2, y: y })] = this.color;
 
-        this.pixels[this.index({ x: x + 0, y: y + 1 })] = this.color;
+        this.pixels[this.index({ x: x, y: y + 1 })] = this.color;
         this.pixels[this.index({ x: x + 2, y: y + 1 })] = this.color;
 
-        this.pixels[this.index({ x: x + 0, y: y + 2 })] = this.color;
+        this.pixels[this.index({ x: x, y: y + 2 })] = this.color;
         this.pixels[this.index({ x: x + 2, y: y + 2 })] = this.color;
 
-        this.pixels[this.index({ x: x + 0, y: y + 3 })] = this.color;
+        this.pixels[this.index({ x: x, y: y + 3 })] = this.color;
         this.pixels[this.index({ x: x + 2, y: y + 3 })] = this.color;
 
-        this.pixels[this.index({ x: x + 0, y: y + 4 })] = this.color;
+        this.pixels[this.index({ x: x, y: y + 4 })] = this.color;
         this.pixels[this.index({ x: x + 1, y: y + 4 })] = this.color;
         this.pixels[this.index({ x: x + 2, y: y + 4 })] = this.color;
     }
 
     /**
+     * Определяет местоположение светодиода по указанным координатам
+     *
+     * @param {{ x: number, y: number }} - позиция по оси `x` и оси `y`
      * max x = 15
      * max y = 9
+     *
+     * @return {number} итоговая позиция светодиода
     */
     index({ x = 0, y = 0 } = {}) {
         const xPos = !(x % 2) ? x * 14: (x - ((x-1)/2)) * 14 * 2 - 1;
         const yPos = !(x % 2) ? y : 0 - y;
-        return xPos + yPos; 
+        return xPos + yPos;
     }
 }
 
